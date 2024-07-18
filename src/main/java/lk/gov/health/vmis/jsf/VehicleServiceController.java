4238f6f39becb6a766de8f65772aa53904930973
Renuka Theminda Kirulapana <131508191+ThemindaKirulapana@users.noreply.github.com>
2024-07-19 04:55:49 +0530

    Signed-off-by: Renuka Theminda Kirulapana <131508191+ThemindaKirulapana@users.noreply.github.com>

--- src/main/java/lk/gov/health/vmis/jsf/VehicleServiceController.java
+++ src/main/java/lk/gov/health/vmis/jsf/VehicleServiceController.java
@@ -4,10 +4,45 @@
  */
 package lk.gov.health.vmis.jsf;
 
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import lk.gov.health.vmis.entities.VehicleService;
+
 /**
  *
  * @author Lenovo
  */
 public class VehicleServiceController {
+
+    private VehicleService vehicleService = new VehicleService();
+    @EJB 
+    private  lk.gov.health.vmis.beans.VehicleServiceFacade ejbFacade;
+    
+ 
+  //------------------------------------------ Insert data
+    
+      public VehicleService getvehicleservise(){
+          
+          if (vehicleService==null){
+              
+              vehicleService=new VehicleService();
+          }
+          return vehicleService;
+      }
+      
+      public void setvehicleservise(){
+          
+          this.vehicleService=vehicleService;
+      }
+      
+      
+    
+    public void insertdayta(){
+        
+       ejbFacade.create(vehicleService);
+    }
+    
+    //---------------------------------------------------------
     
 }
