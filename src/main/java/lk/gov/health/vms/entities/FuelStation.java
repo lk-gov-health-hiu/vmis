4d42b96207287f323b3013e3dca1ef9a91c26152
Renuka Theminda Kirulapana <131508191+ThemindaKirulapana@users.noreply.github.com>
2024-09-05 14:00:09 +0530

    Signed-off-by: Renuka Theminda Kirulapana <131508191+ThemindaKirulapana@users.noreply.github.com>

--- src/main/java/lk/gov/health/vms/entities/FuelStation.java
+++ src/main/java/lk/gov/health/vms/entities/FuelStation.java
@@ -15,19 +15,19 @@ import javax.persistence.Id;
  * @author Lenovo
  */
 @Entity
-public class FuelStationFacade implements Serializable {
+public class FuelStation implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     String name;
+    int dealernumber;
+    String distric;
     String Address;
-    Integer contact;
+    int contact;   
+    int whatsapp;
     String email;
-    Integer whatsapp;
-    
-   
 
     public Long getId() {
         return id;
@@ -36,6 +36,65 @@ public class FuelStationFacade implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDealernumber() {
+        return dealernumber;
+    }
+
+    public void setDealernumber(int dealernumber) {
+        this.dealernumber = dealernumber;
+    }
+
+    public String getDistric() {
+        return distric;
+    }
+
+    public void setDistric(String distric) {
+        this.distric = distric;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public int getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(int whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+   
 
     @Override
     public int hashCode() {
@@ -47,10 +106,10 @@ public class FuelStationFacade implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof FuelStationFacade)) {
+        if (!(object instanceof FuelStation)) {
             return false;
         }
-        FuelStationFacade other = (FuelStationFacade) object;
+        FuelStation other = (FuelStation) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
---
+++ src/main/java/lk/gov/health/vms/jsf/FuelstationController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package lk.gov.health.vms.jsf;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Theminda
+ */
+@Named(value = "fuelstationController")
+@SessionScoped
+public class FuelstationController implements Serializable {
+
+    /**
+     * Creates a new instance of FuelstationController
+     */
+    public FuelstationController() {
+    }
+    
+}
